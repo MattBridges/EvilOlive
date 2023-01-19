23d75a4cc15d7dfe518dac4577ece360d40e881b
MattBridges <Matthew.Bridges1@gmail.com>
2023-01-18 22:18:38 -0500

    Updated Footer Links

--- js/site.js
+++ js/site.js
@@ -3,7 +3,7 @@ function GetValues() {
   //Get the input string
   let inputString = document.getElementById('userString').value;
 
-  DisplayResults(CheckForPalindrome(inputString));
+  DisplayResults(inputString,CheckForPalindrome(inputString));
 }
 
 //Logic Function
@@ -12,32 +12,33 @@ function CheckForPalindrome(stringToCheck) {
   let cleanedString = stringToCheck.replace(/[^a-z0-9]/gi, '').toLowerCase();
 
   //Iterate from outside to inside to see if input is palindrome and stop if not.
-  for(let i = 0; i<=cleanedString.length/2;i++){
+  for(let i = 0; i<=(cleanedString.length/2);i++){
     if(cleanedString[i] != cleanedString[cleanedString.length-i-1]){
       //Not a palindrome
-      return {str:cleanedString.reverse().join(''),isPalindrome:false};
+      return false;
     }    
   }
   //If we make it through the check the word is a palindrome return as such
-  return {str:cleanedString.reverse().join(''),isPalindrome:true};
+  return true;
 }
 
 //View Function
-function DisplayResults(result) {
+function DisplayResults(inputString, result) {
   let alertElement = document.getElementById('alert');
   let alertHeading = document.getElementById('alertHeading');
   let results = document.getElementById('results');
+  let inputReversed = inputString.replace(/[^a-z0-9]/gi, '').toLowerCase().split('').reverse().join('');
 
-  if (result.isPalindrome) {
+  if (result) {
     alertElement.classList.replace('alert-danger', 'alert-success');
     alertHeading.textContent = 'Well Done!You entered a Palindrome';
-    results.textContent = `Your phrase reversed is: ${result.str}`;
+    results.textContent = `Your phrase reversed is: ${inputReversed}`;
     alertElement.classList.remove('invisible');
   }
   else {
     alertElement.classList.replace('alert-success', 'alert-danger');
     alertHeading.textContent = 'Sorry, you did not enter a Palindrome!';
-    results.textContent = `Your phrase reversed is: ${result.str}`;
+    results.textContent = `Your phrase reversed is: ${inputReversed}`;
     alertElement.classList.remove('invisible');
   }
 }
